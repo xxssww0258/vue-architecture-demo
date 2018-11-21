69bf3c5acdcab7a40f5128884f706f2c18e2625b
John <xxssww0258@sina.com>
2018-11-21 16:36:54 +0800

    语法错误

--- src/axios/index.js
+++ src/axios/index.js
@@ -29,7 +29,8 @@ newAxios.interceptors.response.use(function (response) {
     let data = response.data
     // 这里用来统一 不同的后端返回的数据类型
     // 有些后端喜欢返回错 data.errCode  有些喜欢返回 http.status
-    if (data.isSuccess === 1) { return Promise.reject('error') }
+    if (data.isSuccess === 1) { return Promise.reject(Error('error')) }
+
     return data //  改写  data 后就能直接通过 response 获取data了
 }, function (error) {
     // 统一的错误 回馈 一般弹窗口
